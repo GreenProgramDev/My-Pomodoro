3f3baf90a4c0eecb07d94455eb1e685b8aebc8de
Sdney Lima <lima.rosimao@gmail.com>
2023-07-27 01:38:47 -0300

    fix: don't let the timer go negative feat: timer input in minutes

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const display = document.getElementById("time");
+const display = document.getElementById('time');
 const timeTemp = document.getElementById('temporizador')
+const btnStart = document.getElementById('start')
+const btnBreak = document.getElementById('break')
+// const btnStart = document.getElementById('start')
+
 
 let intervalId;
-let isRunning = false
-let seconds = -1
-let minutes = 0
+let isRunning = false;
+let seconds = 0;
+let minutes = 0;
+let limitMinutes = 99;
 
 const updateTime = () => {
   seconds++;
@@ -12,26 +17,68 @@ const updateTime = () => {
   if (seconds >= 60) {
     seconds = 0;
     minutes++;
-  }
+  };
+  if(minutes >= limitMinutes){
+    clearInterval(intervalId);
+    isRunning = false;
+  };
 
   display.textContent =
     (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
+};
+
+const downgradeTime = () => {
+    seconds--;
+  
+    if (seconds <= 0) {
+      seconds = 60;
+      minutes--;
+    };
+
+    if(minutes < 0){
+        clearInterval(intervalId)
+        isRunning = false;
+        seconds = 0;
+        minutes = 0;
+    };
+  
+    display.textContent =
+      (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
 };
 
 const startTime = () => {
     if(!isRunning){
         intervalId = setInterval(updateTime, 1000)
         isRunning = true
-    }
+    };
+    
+    // if()
+    
+};
 
-}
+
+
+const temp = () => {
+    intervalId = setInterval(downgradeTime, 1000)
+    if(timeTemp.value){
+        intervalId = setInterval(downgradeTime, 1000)
+        minutes = timeTemp.value
+        seconds = 0
+        isRunning = true
+        display.textContent = 
+        (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
+        timeTemp.value = ''
+        isRunning = true
+    };
+    
+};
 
 const breakTime = () => {
     if(isRunning){
         clearInterval(intervalId)
         isRunning = false
-    }
-}
+    };
+};
 
 const resetTime = () => {
     clearInterval(intervalId)
@@ -41,28 +88,4 @@ const resetTime = () => {
     isRunning = false
 }
 
-const downgradeTime = () => {
-    seconds--;
-  
-    if (seconds <= 0) {
-      seconds = 60;
-      minutes--;
-    }
-  
-    display.textContent =
-      (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
-  };
 
-const temp = () => {
-    if(timeTemp.value){
-        minutes = timeTemp.value
-        seconds = 0
-        isRunning = true
-        display.textContent = 
-        (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
-        timeTemp.value = ''
-        intervalId = setInterval(downgradeTime, 1000)
-        isRunning = true
-    }
-    
-}
