fad52b9208b78783aa5f42e7f81debbe58eb55cc
Sdney Lima <lima.rosimao@gmail.com>
2023-07-31 22:24:11 -0300

    feat: btn Stopwatch receives from input as timer limiter and timer receives from input as countdown timer

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const display = document.getElementById('time');
-const timeTemp = document.getElementById('temporizador')
-const btnStart = document.getElementById('start')
-const btnBreak = document.getElementById('break')
-// const btnStart = document.getElementById('start')
-
+const display = document.getElementById("time");
+const timeTemp = document.getElementById("temporizador");
+const btnStart = document.getElementById("start");
+const btnBreak = document.getElementById("break");
 
 let intervalId;
 let isRunning = false;
 let seconds = 0;
 let minutes = 0;
-let limitMinutes = 99;
+let limitMinutes = 60;
 
 const updateTime = () => {
   seconds++;
@@ -17,75 +15,99 @@ const updateTime = () => {
   if (seconds >= 60) {
     seconds = 0;
     minutes++;
-  };
-  if(minutes >= limitMinutes){
+  }
+  if (minutes >= limitMinutes) {
     clearInterval(intervalId);
     isRunning = false;
-  };
+  }
 
   display.textContent =
-    (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
+    (minutes < 10 ? "0" : "") +
+    minutes +
+    ":" +
+    (seconds < 10 ? "0" : "") +
+    seconds;
 };
 
 const downgradeTime = () => {
-    seconds--;
-  
-    if (seconds <= 0) {
-      seconds = 60;
-      minutes--;
-    };
+  seconds--;
 
-    if(minutes < 0){
-        clearInterval(intervalId)
-        isRunning = false;
-        seconds = 0;
-        minutes = 0;
-    };
-  
-    display.textContent =
-      (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
+  if (seconds <= 0) {
+    seconds = 60;
+    minutes--;
+  }
+
+  if (minutes < 0) {
+    clearInterval(intervalId);
+    isRunning = false;
+    seconds = 0;
+    minutes = 0;
+  }
+
+  display.textContent =
+    (minutes < 10 ? "0" : "") +
+    minutes +
+    ":" +
+    (seconds < 10 ? "0" : "") +
+    seconds;
 };
 
 const startTime = () => {
-    if(!isRunning){
-        intervalId = setInterval(updateTime, 1000)
-        isRunning = true
-    };
-    
-    // if()
-    
+  if (downgradeTime) {
+    clearInterval(intervalId);
+    isRunning = false;
+  }
+  if (!isRunning) {
+    intervalId = setInterval(updateTime, 1000);
+    isRunning = true;
+  }
+  if (timeTemp.value) {
+    limitMinutes = timeTemp.value;
+  }
 };
 
-
-
 const temp = () => {
-    intervalId = setInterval(downgradeTime, 1000)
-    if(timeTemp.value){
-        intervalId = setInterval(downgradeTime, 1000)
-        minutes = timeTemp.value
-        seconds = 0
-        isRunning = true
-        display.textContent = 
-        (minutes < 10 ? "0" : '') + minutes + ":" + (seconds < 10 ? "0" : '') + seconds;
-        timeTemp.value = ''
-        isRunning = true
-    };
-    
+  breakTime();
+  intervalId = setInterval(downgradeTime, 1000);
+
+  if (timeTemp.value) {
+    breakTime()
+    intervalId = setInterval(downgradeTime, 1000);
+    minutes = timeTemp.value;
+    seconds = 0;
+    isRunning = true;
+    display.textContent =
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 10 ? "0" : "") +
+      seconds;
+    timeTemp.value = "";
+    // isRunning = true;
+  }
 };
 
 const breakTime = () => {
-    if(isRunning){
-        clearInterval(intervalId)
-        isRunning = false
-    };
+  if (isRunning) {
+    clearInterval(intervalId);
+    isRunning = false;
+  }
+  if (downgradeTime) {
+    clearInterval(intervalId);
+    isRunning = false;
+  }
 };
 
 const resetTime = () => {
-    clearInterval(intervalId)
-    seconds = 0
-    minutes = 0
-    display.textContent = ''
-    isRunning = false
-}
-
-
+  clearInterval(intervalId);
+  seconds = 0;
+  minutes = 0;
+  if (startTime) {
+    isRunning = false;
+  }
+  display.textContent = "";
+  if (downgradeTime) {
+    clearInterval(intervalId);
+    isRunning = false;
+  }
+};
